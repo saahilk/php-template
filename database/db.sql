349e75066885fefa5460e32083af13eb11ec145b
swaroop-kaza <kazasrisaiswaroop@gmail.com>
2018-03-21 21:07:44 +0530

    heroku

--- database/db.sql
+++ database/db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.6deb4
--- https://www.phpmyadmin.net/
+-- version 4.5.4.1deb2ubuntu2
+-- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 21, 2018 at 07:08 PM
--- Server version: 5.7.20-0ubuntu0.17.04.1
--- PHP Version: 7.0.22-0ubuntu0.17.04.1
+-- Generation Time: Mar 21, 2018 at 01:10 PM
+-- Server version: 5.7.21-0ubuntu0.16.04.1
+-- PHP Version: 7.0.28-1+ubuntu16.04.1+deb.sury.org+1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,110 +17,40 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `deanproject`
+-- Database: `track`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `history`
+-- Table structure for table `user`
 --
 
-CREATE TABLE `history` (
-  `no` int(255) NOT NULL,
-  `rollno` varchar(255) NOT NULL,
-  `status` varchar(255) NOT NULL,
-  `datemodify` varchar(225) NOT NULL
+CREATE TABLE `user` (
+  `emailid` varchar(90) DEFAULT NULL,
+  `flag` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `history`
+-- Dumping data for table `user`
 --
 
-INSERT INTO `history` (`no`, `rollno`, `status`, `datemodify`) VALUES
-(1, 'b150223cs', 'option1', '2018-03-22'),
-(3, 'b150005cs', 'option3', '2018-03-22'),
-(4, 'b150223cs', 'option3', '2018-03-29');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `mtechstudent`
---
-
-CREATE TABLE `mtechstudent` (
-  `name` varchar(255) NOT NULL,
-  `rollno` varchar(255) NOT NULL,
-  `email` varchar(225) NOT NULL,
-  `department` varchar(225) NOT NULL,
-  `guide` varchar(225) NOT NULL,
-  `guidemail` varchar(225) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `mtechstudent`
---
-
-INSERT INTO `mtechstudent` (`name`, `rollno`, `email`, `department`, `guide`, `guidemail`) VALUES
-('heshan', 'b150005cs', 'deshawn@gmail.com', 'Mathematics', 'vinod', 'sumanathilaka_b150413cs@nitc.ac.in'),
-('suneet', 'b150223cs', 'ara@gmail.com', 'Mathematics', 'vinod', 'sumanathilaka_b150413cs@nitc.ac.in'),
-('deshan', 'b150413cs', 'deshan@gmail.com', 'Computer Science & Engineering', 'vinod', 'sumanathilaka_b150413cs@nitc.ac.in'),
-('rakhee', 'b150474cs', 'rakhee@gmail.com', 'Mechanical Engineering', 'vinod', 'sumanathilaka_b150413cs@nitc.ac.in');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `project`
---
-
-CREATE TABLE `project` (
-  `rollno` varchar(225) NOT NULL,
-  `topic` varchar(225) NOT NULL,
-  `status` varchar(225) NOT NULL,
-  `date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `project`
---
-
-INSERT INTO `project` (`rollno`, `topic`, `status`, `date`) VALUES
-('b150005cs', 'solar plant', 'option3', '2018-03-22'),
-('b150223cs', 'asus', 'option3', '2018-03-29'),
-('b150413cs', 'network', 'option1', '2018-03-22'),
-('b150474cs', 'compiler', 'option1', '2018-03-14');
+INSERT INTO `user` (`emailid`, `flag`) VALUES
+('saiswaroop_b150376cs@nitc.ac.in', 1),
+('maheshkumar_b150588cs@nitc.ac.in', 2),
+('saahilsuhas_b150470cs@nitc.ac.in', 3);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `history`
+-- Indexes for table `user`
 --
-ALTER TABLE `history`
-  ADD PRIMARY KEY (`no`);
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`flag`),
+  ADD UNIQUE KEY `flag` (`flag`);
 
---
--- Indexes for table `mtechstudent`
---
-ALTER TABLE `mtechstudent`
-  ADD PRIMARY KEY (`rollno`);
-
---
--- Indexes for table `project`
---
-ALTER TABLE `project`
-  ADD PRIMARY KEY (`rollno`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `history`
---
-ALTER TABLE `history`
-  MODIFY `no` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
