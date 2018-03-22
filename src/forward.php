554d8acad26020ce8ecd23c9e70de8b9ecd39c41
saahilk <kamatsaahil@gmail.com>
2018-03-22 23:16:57 +0530

    forwarding 1.1.1

---
+++ src/forward.php
@@ -0,0 +1,32 @@
+<?php
+
+session_start();
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$servername = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+// Create connection
+$conn = new mysqli($servername, $username, $password, $db);
+
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
+if($_SESSION['user_id']=="") {
+  	header('Location:index.php');
+}
+
+$people=[
+	0=>"Staff",
+	1=>"Dean",
+	2=>"DR",
+	3=>"GA1",
+	4=>"GA2",
+	5=>"GA3",
+	6=>"GA4",
+	7=>"GA5",
+];
+
+$ind=''.array_keys($_POST)[0];
+echo $ind;
+?>
