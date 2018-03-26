831b33026778804dfd397a5d6d08a3b7fce0e857
swaroop-kaza <kazasrisaiswaroop@gmail.com>
2018-03-27 02:51:08 +0530

    timestamp4

--- src/home.php
+++ src/home.php
@@ -256,7 +256,7 @@ echo $people[$_SESSION['user_id']]
   {
     $ID=$row_blog['ID'];
     $timestamp1=$row_blog['recieved_on'];
-    $timestamp3=$timestamp1->format('d-m-Y|H:i');
+    $timestamp3=date_format($timestamp1,"d/m/Y H:i");
     $remarks=$row_blog['remarks'];
     $submitted_by=$row_blog['submitted_by'];
     $timestamp2=$row_blog['submitted_on'];
@@ -268,7 +268,7 @@ echo $people[$_SESSION['user_id']]
   
   
   <td>'.$ID.'</td>
-  <td>'.$timestamp1.'</td>
+  <td>'.$timestamp3.'</td>
   <td>'.$remarks.'</td>
   <td>'.$submitted_by.'</td>
   <td>'.$timestamp2.'</td>
