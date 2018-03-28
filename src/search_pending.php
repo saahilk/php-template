54f4445f98ab74cbbe88fb3687f60252892f312b
chinnu3981 <maheshkumar.tangella@gmail.com>
2018-03-28 17:08:04 +0530

    search button 1.1

--- src/search_pending.php
+++ src/search_pending.php
@@ -53,6 +53,8 @@ $user_id=$_SESSION['user_id'];
 <html>
 	<head>
 		<meta name="google-signin-client_id" content="952937888060-b470t00um3s9uclet4qaua91ocvrjfus.apps.googleusercontent.com">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 		<script src="https://apis.google.com/js/platform.js" async defer></script>
 
 		<script>
@@ -122,6 +124,42 @@ $user_id=$_SESSION['user_id'];
 
 			b{
 			  	font-weight: bold;
+			}
+
+
+			* {
+    			box-sizing: border-box;
+			}
+
+			form.example input[type=text] {
+			    padding: 10px;
+			    font-size: 17px;
+			    border: 1px solid grey;
+			    float: left;
+			    width: 80%;
+			    background: #f1f1f1;
+			}
+
+			form.example button {
+			    float: left;
+			    width: 20%;
+			    padding: 10px;
+			    background: #2196F3;
+			    color: white;
+			    font-size: 17px;
+			    border: 1px solid grey;
+			    border-left: none;
+			    cursor: pointer;
+			}
+
+			form.example button:hover {
+			    background: #0b7dda;
+			}
+
+			form.example::after {
+			    content: "";
+			    clear: both;
+			    display: table;
 			}
 		</style>
 	</head>
