7ef7c52fcc36a09e956bd229c498474dfb44be40
chinnu3981 <maheshkumar.tangella@gmail.com>
2018-03-28 15:51:33 +0530

    search button

--- src/home.php
+++ src/home.php
@@ -124,6 +124,42 @@ $user_id=$_SESSION['user_id'];
 			b{
 			  	font-weight: bold;
 			}
+
+
+			* {
+    			box-sizing: border-box;
+			}
+
+			form.example input[type=text] {
+			    padding: 10px;
+			    font-size: 17px;
+			    border: 1px solid grey;
+			    float: left;
+			    width: 80%;
+			    background: #f1f1f1;
+			}
+
+			form.example button {
+			    float: left;
+			    width: 20%;
+			    padding: 10px;
+			    background: #2196F3;
+			    color: white;
+			    font-size: 17px;
+			    border: 1px solid grey;
+			    border-left: none;
+			    cursor: pointer;
+			}
+
+			form.example button:hover {
+			    background: #0b7dda;
+			}
+
+			form.example::after {
+			    content: "";
+			    clear: both;
+			    display: table;
+			}
 		</style>
 	</head>
 
@@ -181,6 +217,14 @@ echo $people[$_SESSION['user_id']]
 		    	</ul>
 		</div>
 	<center>
+
+
+		<form class="example" action="#" style="margin:auto;max-width:300px">
+  <input type="text" placeholder="Search.." name="search2">
+  <button type="submit"><i class="fa fa-search"></i></button>
+</form>
+
+
 		<?php
 				if($_SESSION['user_id']=='0'){
 					?>
